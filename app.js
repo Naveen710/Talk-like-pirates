cae26a2302fa03b837b8c8ef1ce93dd533e160fc
Naveen710 <59551835+Naveen710@users.noreply.github.com>
2022-08-27 14:30:21 +0530

    update:initial relase

---
+++ app.js
@@ -0,0 +1,29 @@
+/*var userName=prompt("May I know your name please")
+alert("My name is" + userName)*/
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var Output = document.querySelector("#output")
+
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" +encodeURIComponent(text);
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something went wrong with server")
+}
+
+function clickHandler() {
+    var inputTxt = txtInput.value;
+
+    fetch(getTranslationUrl(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            Output.innerText = pirateTranslatedText;
+            console.log(json.contents.translated);
+        })
+        .catch(errorHandler)
+    }
+    btnTranslate.addEventListener("click", clickHandler);
