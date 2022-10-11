2b67c9b5ecf37c5701167f80e562b07595a1616e
Naveen710 <59551835+Naveen710@users.noreply.github.com>
2022-10-11 22:00:13 +0530

    final commit

--- app.js
+++ app.js
@@ -2,28 +2,29 @@
 alert("My name is" + userName)*/
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
-var Output = document.querySelector("#output")
+var txtOutput = document.querySelector("#txt-output")
 
-var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
+var serverUrl = 'https://api.funtranslations.com/translate/yoda.json';
 
 function getTranslationUrl(text) {
-    return serverUrl + "?" + "text=" +encodeURIComponent(text);
+    return serverUrl + "?" + "text=" + text;
 }
-function errorHandler(error){
-    console.log("error occured",error);
-    alert("something went wrong with server")
+
+function errorHandler(error) {
+    console.log(error);
+    alert('Something is wrong with server');
 }
 
 function clickHandler() {
     var inputTxt = txtInput.value;
 
+
     fetch(getTranslationUrl(inputTxt))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            Output.innerText = pirateTranslatedText;
-            console.log(json.contents.translated);
+                txtOutput.innerHTML = translatedText;
         })
         .catch(errorHandler)
-    }
-    btnTranslate.addEventListener("click", clickHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
